502a1a0cd77db1681b35cdec988814f1fda7acad
Varadharajan Dhanasekaran <dvaradha@gmail.com>
2021-11-09 12:18:59 +0530

    my first code

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+    project = "terraform-287414"
+    region = "us-central1"
+}
+
+resource "google_compute_instance" "myvm" {
+        name = "myfirstvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
+
+
+resource "google_compute_instance" "myvm1" {
+        name = "myfirstvm1"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
