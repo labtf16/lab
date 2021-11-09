995791f48a278e39dc6193cd8d571f3b89a6d399
tflab16 <tflab16@gmail.com>
2021-11-09 12:40:02 +0530

    thirdvm

--- main.tf
+++ main.tf
@@ -31,3 +31,18 @@ resource "google_compute_instance" "myvm1" {
                 }
         }
 }
+
+resource "google_compute_instance" "myvm3" {
+        name = "thirdvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+                }
+        }
+}
+
